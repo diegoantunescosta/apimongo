ab517bc98b5a34377811ee05eada73b08edbdf21
Diego Antunes <54458716+diegoantunescosta@users.noreply.github.com>
2021-11-22 17:18:35 -0300

    Update user.js

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -10,8 +10,8 @@ var transporter = nodemailer.createTransport({
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
-        user: 'diegoantunescosta@gmail.com', // generated ethereal user
-        pass: '145677$$' // generated ethereal password
+        user: 'mentto.edu@gmail.com', // generated ethereal user
+        pass: 'Mentto2020' // generated ethereal password
     },
     tls: {
         rejectUnauthorized: false 
@@ -60,7 +60,7 @@ exports.esqueciSenha = function(req, res, next) {
             `;
 
             var mailOptions = {
-                from: 'diegoantunescosta@gmail.com',
+                from: 'octopusprojectoficial@gmail.com',
                 to: email,
                 subject: 'Recuperação de Conta Octopus Project',
                 html: conteudo_email
